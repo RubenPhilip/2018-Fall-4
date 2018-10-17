1a1e2c45207213d61797782c3d587e19ec2ac097
ms pais <42386796+mspais@users.noreply.github.com>
2018-10-17 10:37:41 -0400

    JS demo

--- JS/demo.js
+++ JS/demo.js
@@ -1,1 +1,14 @@
+function A1(){
+	var fName = prompt("Hey, what's your name?", name);
+	var myColour = prompt("Pick a colour");
 
+	if (myColour == "red"){
+		console.log("Looks like this guy likes red. Noted")
+	} else if (myColour == "yellow"){
+		console.log("This guy chose yellow. Hmm")
+	} else {
+		console.log("That's not a colour")
+	}
+
+
+}
